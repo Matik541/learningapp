8e195c20acd5450ee5222a8e0f6ad12b60ed02d3
Marian Horoshko <marian.horoshko@gmail.com>
2022-10-01 14:51:42 +0200

    Add todo and db files to gitignore. Add typeORM with sqlite to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'LearningApp.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      // change to false in production mode
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
