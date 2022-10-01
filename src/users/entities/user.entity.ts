08d3fcba4e310d6eb275be74c98489cd791886ec
Marian Horoshko <marian.horoshko@gmail.com>
2022-10-01 14:53:38 +0200

    Add some parameters to user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,18 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class User {}
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  userName: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  hashedPassword: string;
+
+  //   TODO: add lessonsCompleted, lessonsCreated, createdComments
+}
