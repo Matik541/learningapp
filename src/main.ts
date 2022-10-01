3a91a7090f9295a4c3a410c3c765f7890fd2d139
Marian Horoshko <marian.horoshko@gmail.com>
2022-10-01 14:17:00 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  // enable to connect from localhost
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
