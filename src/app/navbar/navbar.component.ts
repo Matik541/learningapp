f8bc654c90cefa146150b04ef2328a21071070f2
Matik Laptop <mateuszkowaslki199700@gmail.com>
2022-10-02 22:15:21 +0200

    scss and navbar theme: deep-purple, green (A), pink (Warn) navnbar: ~60% done (just design) Login/out: interacion "done"

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 /**
  * @title Basic menu
@@ -7,4 +7,31 @@ import {Component} from '@angular/core';
   selector: 'navbar',
   templateUrl: 'navbar.component.html',
 })
-export class NavbarComponent {}
+
+export class NavbarComponent {
+  app = {
+    title: "LearningApp",
+  }
+  
+  loged: null | { name: string; picture: string; } = null;
+  // loged: null | { name: string; picture: string; } = { name: "User name" };
+
+  constructor() { }
+
+  ngOnInit() { 
+    this.Lout();
+  }
+
+  Sin() { 
+    console.log("Sin")
+    this.loged = { name: "User name",  picture: "" };
+  }
+  Sup() { 
+    console.log("Sup") 
+    this.loged = { name: "New user",  picture: "" };
+  }
+  Lout() { 
+    console.log("Lout")
+    this.loged = null;
+   }
+}
