540fbd03232a123bffeabf2107ff032d6c3e68a4
Matik Laptop <mateuszkowaslki199700@gmail.com>
2022-11-23 08:03:19 +0100

    Answers handler in learn (writing) shake animation - wrong, go to next - correct

--- src/app/lessons/lesson/learn/learn.component.ts
+++ src/app/lessons/lesson/learn/learn.component.ts
@@ -1,4 +1,6 @@
+import { F } from '@angular/cdk/keycodes'
 import { Component, Input, OnInit } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
 import { LessonsService } from 'src/app/lessons.service'
 import { ProgressBarService } from 'src/app/navbar/progress-bar.service'
 import { Flashcard, Lesson } from 'src/environments/environment'
@@ -10,36 +12,85 @@ import { Flashcard, Lesson } from 'src/environments/environment'
 })
 export class LearnComponent implements OnInit {
   id: number = 0
-  private lec: any
+  indexFleshcard: number = 0
+  answer: string = ''
+
+  private sub: any
   flashcards: {
     flashcard: Flashcard
+    answer: string
     status: 0 | 1 | 2
-  }[]
-  @Input() lesson: Lesson = {} as Lesson
+  }[] = []
+
   constructor(
     public progressBar: ProgressBarService,
-    private lessonsService: LessonsService
-  ) {}
+    private lessonsService: LessonsService,
+    private activeRoute: ActivatedRoute
+  ) {
+    this.sub = this.activeRoute.params.subscribe((params) => {
+      this.id = +params['id']
+    })
+  }
 
   ngOnInit(): void {
-    this.lec = this.lessonsService.getLesson(this.id).subscribe((data) => {
-      this.flashcards = data.flashcards.map((flashcard) => {
-        return {
-          flashcard,
-          status: 0,
-        }
-      })
+    this.sub = this.lessonsService.getLesson(this.id).subscribe((data) => {
+      this.flashcards = data.flashcards
+        .map((flashcard) => {
+          return {
+            flashcard,
+            answer: '',
+            status: 0 as 0,
+          }
+        })
+        .sort(() => Math.random() - 0.5)
+      console.log(this.flashcards)
     })
-
-    console.log(this.flashcards)
 
     this.progressBar.mode = 'determinate'
     this.progressBar.value = 0
     this.progressBar.hidden = false
   }
 
+  check(): void {
+    let card = document.querySelector('.flashcard')
+
+    card?.classList.remove('correct', 'incorrect')
+    let size = this.flashcards.length
+    let index = this.indexFleshcard
+    if (this.flashcards[index % size].flashcard.answer === this.answer) {
+      this.flashcards[index % size].status++
+      this.flashcards[index % size].answer = this.answer
+      console.log(this.flashcards[index % size])
+      card?.classList.add('correct')
+
+      setTimeout(() => {
+        card?.setAttribute('style', 'animation: none')
+        card?.setAttribute(
+          'style',
+          `animation: slide-right 0.25s ease-in-out 1`
+        )
+
+        card?.addEventListener('animationend', () =>
+          card?.setAttribute('style', 'animation: none')
+        )
+
+        setTimeout(() => {
+          this.indexFleshcard += 1
+          card?.classList.remove('correct', 'incorrect')
+          this.answer = ''
+        }, 125)
+      }, 500)
+    } else {
+      card?.classList.add('incorrect')
+      card?.setAttribute('style', `animation: shake 0.5s ease-in-out 1`)
+    }
+    card?.addEventListener('animationend', () => {
+      card?.setAttribute('style', 'animation: none')
+    })
+  }
+
   ngOnDestroy(): void {
     this.progressBar.hidden = true
-    this.lec.unsubscribe()
+    this.sub.unsubscribe()
   }
 }
