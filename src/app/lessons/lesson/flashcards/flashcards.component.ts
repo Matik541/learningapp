6554786cd692002c72b18fae7b8e13d2bdffadc6
Matik Laptop <mateuszkowaslki199700@gmail.com>
2022-12-09 23:14:46 +0100

    update and discard changes - lesson updates delete - wait for backend

--- src/app/lessons/lesson/flashcards/flashcards.component.ts
+++ src/app/lessons/lesson/flashcards/flashcards.component.ts
@@ -1,3 +1,4 @@
+import { I } from '@angular/cdk/keycodes';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { LessonsService } from 'src/app/lessons.service';
@@ -5,118 +6,116 @@ import { UsersService } from 'src/app/users.service';
 import { Flashcard, Lesson, User } from 'src/environments/environment';
 
 @Component({
-  selector: 'lesson-flashcards',
-  templateUrl: './flashcards.component.html',
-  styleUrls: ['./flashcards.component.scss'],
+	selector: 'lesson-flashcards',
+	templateUrl: './flashcards.component.html',
+	styleUrls: ['./flashcards.component.scss'],
 })
 export class FlashcardsComponent implements OnInit {
-  @Input() lesson: Lesson | undefined = {} as Lesson;
+	@Input() lesson: Lesson | undefined = {} as Lesson;
 
-  logged: User = {} as User;
+	logged: User = {} as User;
 
-  flashcard: Flashcard = {} as Flashcard;
-  indexFleshcard: number = 0;
+	flashcard: Flashcard = {} as Flashcard;
+	indexFleshcard: number = 0;
 
-  edited: boolean = false;
-  id: number = 0;
-  private sub: any;
-  private lec: any;
+	edited: boolean = false;
 
-  constructor(
-    private usersService: UsersService,
-    private activeRoute: ActivatedRoute,
-    private route: Router,
-    private lessonsService: LessonsService
-  ) {
-    this.logged = this.usersService.loggedUser;
+	constructor(
+		private usersService: UsersService,
+		private route: Router,
+		private lessonsService: LessonsService
+	) {
+		this.logged = this.usersService.loggedUser;
+	}
 
-    this.sub = this.activeRoute.params.subscribe((params) => {
-      this.id = +params['id'];
-    });
-  }
+	ngOnInit(): void {
+		setTimeout(() => {
+			if (this.lesson)
+				if (this.lesson.flashcards?.length > 0) this.flashcard = this.lesson.flashcards[0];
+		}, 1);
+	}
 
-  ngOnInit(): void {
-    this.lec = this.lessonsService.getLesson(this.id).subscribe((data) => {
-      if (data != null) {
-        this.lesson = data;
-      }
-    });
-    setTimeout(() => {
-      if (this.lesson)
-        if (this.lesson.flashcards?.length > 0) this.flashcard = this.lesson.flashcards[0];
-    }, 1);
-  }
+	flipFlashcard(): void {
+		let card = document.querySelector('.flashcard');
+		let front = document.querySelector('.front');
+		let back = document.querySelector('.back');
+		card?.setAttribute('style', 'animation: none');
+		card?.setAttribute('style', 'animation: flip 0.5s 1');
 
-  flipFlashcard(): void {
-    let card = document.querySelector('.flashcard');
-    let front = document.querySelector('.front');
-    let back = document.querySelector('.back');
-    card?.setAttribute('style', 'animation: none');
-    card?.setAttribute('style', 'animation: flip 0.5s 1');
+		card?.addEventListener('animationend', () => card?.setAttribute('style', 'animation: none'));
 
-    card?.addEventListener('animationend', () => card?.setAttribute('style', 'animation: none'));
+		setTimeout(() => {
+			front?.classList.toggle('hidden');
+			back?.classList.toggle('hidden');
+		}, 250);
+	}
 
-    setTimeout(() => {
-      front?.classList.toggle('hidden');
-      back?.classList.toggle('hidden');
-    }, 250);
-  }
+	changeFlashcard(by: -1 | 1): void {
+		let card = document.querySelector('.flashcard');
 
-  changeFlashcard(by: -1 | 1): void {
-    let card = document.querySelector('.flashcard');
+		card?.setAttribute('style', 'animation: none');
+		card?.setAttribute(
+			'style',
+			`animation: slide-${by == -1 ? 'left' : 'right'} 0.25s ease-in-out 1 `
+		);
 
-    card?.setAttribute('style', 'animation: none');
-    card?.setAttribute(
-      'style',
-      `animation: slide-${by == -1 ? 'left' : 'right'} 0.25s ease-in-out 1 `
-    );
+		card?.addEventListener('animationend', () => card?.setAttribute('style', 'animation: none'));
 
-    card?.addEventListener('animationend', () => card?.setAttribute('style', 'animation: none'));
+		setTimeout(() => {
+			document.querySelector('.front')?.classList.remove('hidden');
+			document.querySelector('.back')?.classList.add('hidden');
+			if (this.lesson) {
+				this.indexFleshcard += by;
+				if (this.indexFleshcard >= 0 && this.indexFleshcard < this.lesson.flashcards.length) {
+					this.flashcard = this.lesson.flashcards[this.indexFleshcard];
+				}
+			}
+		}, 125);
+	}
 
-    setTimeout(() => {
-      document.querySelector('.front')?.classList.remove('hidden');
-      document.querySelector('.back')?.classList.add('hidden');
-      if (this.lesson) {
-        this.indexFleshcard += by;
-        if (this.indexFleshcard >= 0 && this.indexFleshcard < this.lesson.flashcards.length) {
-          this.flashcard = this.lesson.flashcards[this.indexFleshcard];
-        }
-      }
-    }, 125);
-  }
+	editFlashcard(id: number, intent: 'edit' | 'save' | 'cancel'): void {
+		let buttons = {
+			edit: document.querySelectorAll('mat-card-content > .action > .edit')[id],
+			save: document.querySelectorAll('mat-card-content > .action > .save')[id],
+			// cancel: document.querySelectorAll('mat-card-content > .action > .cancel')[id],
+		};
+		let flashcard = document.querySelectorAll('mat-card-content > .content')[id];
+		let editForm = document.querySelectorAll('mat-card-content > .editForm')[id];
 
-  editFlashcard(id: number, intent: 'edit' | 'save' | 'cancel'): void {
-    let buttons = {
-      edit: document.querySelectorAll('mat-card-content > .action > .edit')[id],
-      save: document.querySelectorAll('mat-card-content > .action > .save')[id],
-      cancel: document.querySelectorAll('mat-card-content > .action > .cancel')[id],
-    };
-    let flashcard = document.querySelectorAll('mat-card-content > .content')[id];
-    let editForm = document.querySelectorAll('mat-card-content > .editForm')[id];
+		if (intent == 'edit') {
+			buttons.edit?.classList.add('hide');
+			flashcard?.classList.add('hide');
+			editForm?.classList.remove('hide');
+			buttons.save?.classList.remove('hide');
+		} else {
+			buttons.save?.classList.add('hide');
+			editForm?.classList.add('hide');
+			buttons.edit?.classList.remove('hide');
+			flashcard?.classList.remove('hide');
+			this.edited = intent == 'save' || this.edited;
+		}
+	}
 
-    console.log(buttons.edit, buttons.save, buttons.cancel, flashcard, editForm);
+	deleteLesson() {
+		if (this.lesson)
+			this.lessonsService.delete(this.lesson.id).subscribe(() => {
+				console.log(this.lesson);
+			});
+		// this.route.navigate(['/lessons']);
+	}
 
-    if (intent == 'edit') {
-      buttons.edit?.classList.add('hide');
-      flashcard?.classList.add('hide');
-      editForm?.classList.remove('hide');
-      buttons.cancel?.classList.remove('hide');
-      buttons.save?.classList.remove('hide');
-    } else {
-      buttons.cancel?.classList.add('hide');
-      buttons.save?.classList.add('hide');
-      editForm?.classList.add('hide');
-      buttons.edit?.classList.remove('hide');
-      flashcard?.classList.remove('hide');
-      this.edited = intent == 'save';
-    }
-  }
+	refreshLesson() {
+		// refresh this component
+		this.route.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+			if (this.lesson) this.route.navigate(['/lessons', this.lesson.id]);
+		});
+	}
 
-  deleteLesson() {}
-
-  cancelLesson() {
-    this.route.navigate(['']);
-  }
-
-  updateLesson() {}
+	updateLesson() {
+		console.log(this.lesson, this.lesson?.id);
+		if (this.lesson)
+			this.lessonsService.update(this.lesson.id, this.lesson).subscribe(() => {
+				if (this.edited) this.refreshLesson();
+			});
+	}
 }
