35873b9c821fc7196a6bf52850c035cba95e8ca3
Matik - PC <mateuszkowalski199700@gmail.com>
2023-10-12 20:46:20 +0200

    add env values

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+}
+export const appName = 'QuizUp'
+
+export const API_URL = 'http://localhost:3000'
+
+export const refreshToken = 'refreshToken'
+export const accessToken = 'accessToken'
