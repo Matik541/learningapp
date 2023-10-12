4e60a0c6cbb2587bd6411e9447a438359f28284b
Matik - PC <mateuszkowalski199700@gmail.com>
2023-10-12 20:45:43 +0200

    add some enums

---
+++ src/app/enums/enums.ts
@@ -0,0 +1,42 @@
+export type Tag = {
+  id: Number
+  tagName: String
+}
+
+export type Flashcard =
+  | {
+      id: number
+      question: string
+      answer: string
+    }
+  | {
+      question: string
+      answer: string
+    }
+
+export type Comment = {
+  id: number
+  comment: string
+  creator: User
+}
+
+export type User = null | {
+  userName: string
+  id: number
+}
+export type Lesson = {
+  id: number
+  title: string
+  description: string
+  iconPath: string
+  creator: User
+  flashcards: Flashcard[]
+  tags: Tag[]
+  comments: Comment[]
+}
+
+export type Bar = {
+  hidden: boolean
+  mode: 'indeterminate' | 'query' | 'determinate' | 'buffer'
+  value: number
+}
