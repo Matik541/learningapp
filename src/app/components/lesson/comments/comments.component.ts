5ba08800bcab10b4cb9b98b2a5d1d6273b3a8e7f
Matik Laptop <mateuszkowaslki199700@gmail.com>
2023-10-19 15:15:06 +0200

    comment functionalyty and paginator

--- src/app/components/lesson/comments/comments.component.ts
+++ src/app/components/lesson/comments/comments.component.ts
@@ -1,17 +1,40 @@
 import { Component, Input } from '@angular/core';
-import { Comment } from 'src/app/enums/enums'
+import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Comment } from 'src/app/enums/enums';
+import { CommentsService } from 'src/app/services/comments.service';
 
 @Component({
   selector: 'lesson-comments',
   templateUrl: './comments.component.html',
-  styleUrls: ['./comments.component.scss']
+  styleUrls: ['./comments.component.scss'],
 })
 export class CommentsComponent {
-  
   @Input() comments: Comment[] | undefined;
 
-    constructor(
-    ) { 
+  lowBound: number = 0;
+  highBound: number = 20;
 
+  comment: string = '';
+
+  constructor(private commentsService: CommentsService, private snackBar: MatSnackBar) {}
+
+  public getPaginatorData(event: PageEvent): PageEvent {
+    this.lowBound = event.pageIndex * event.pageSize;
+    this.highBound = this.lowBound + event.pageSize;
+    return event;
+  }
+
+  public send() {
+    if (
+      this.comment === '' ||
+      this.comment === null ||
+      this.comment === undefined 
+    ) {
+      
+      return;
     }
+    this.commentsService.addComment(this.comment);
+    this.comment = '';
+  }
 }
