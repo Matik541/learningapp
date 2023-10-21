7a9a7fdd7802b99445793fbe04f1afa8c868c8fd
Marian Horoshko <marian.horoshko@gmail.com>
2023-10-21 16:39:23 +0200

    clean user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { ApiProperty } from '@nestjs/swagger';
 
 // entities
 import { Lesson } from '../../lessons/entities/lesson.entity';
@@ -8,16 +7,13 @@ import { LessonCompleted } from '../../lessons/entities/lessonCompleted.entity';
 
 @Entity()
 export class User {
-  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
   // TODO: rename
-  @ApiProperty()
   @Column()
   userName: string;
 
-  @ApiProperty()
   @Column({ unique: true })
   email: string;
 
@@ -27,41 +23,12 @@ export class User {
   @Column({ nullable: true })
   hashedRefreshToken?: string;
 
-  @ApiProperty({
-    type: () => [Lesson],
-    example: [
-      {
-        id: 0,
-        title: 'string',
-        description: 'string',
-        iconPath: 'string',
-      },
-    ],
-  })
   @OneToMany(() => Lesson, (lesson) => lesson.creator)
   createdLessons: Lesson[];
 
-  @ApiProperty({
-    type: () => [Comment],
-    example: [
-      {
-        id: 0,
-        comment: 'string',
-      },
-    ],
-  })
   @OneToMany(() => Comment, (comment) => comment.creator)
   comments: Comment[];
 
-  @ApiProperty({
-    type: () => [LessonCompleted],
-    example: [
-      {
-        id: 0,
-        score: 0,
-      },
-    ],
-  })
   @OneToMany(() => LessonCompleted, (lessonCompleted) => lessonCompleted.user)
   lessonsCompleted: LessonCompleted[];
 }
