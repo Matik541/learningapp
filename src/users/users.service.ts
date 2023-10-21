cca9ce1db818099a4d26e178029176dfb40c8e4f
Marian Horoshko <marian.horoshko@gmail.com>
2023-10-21 16:36:56 +0200

    add getMe method to users service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { FindOptionsRelations, FindOptionsSelect, Repository } from 'typeorm';
 
 // entities
 import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  private readonly userSelect: FindOptionsSelect<User> = {
+    id: true,
+    userName: true,
+    email: true,
+    createdLessons: true,
+    comments: true,
+  };
+
+  private readonly userRelationships: FindOptionsRelations<User> = {
+    createdLessons: true,
+    comments: true,
+  };
+
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
 
+  async getMe(userId: number): Promise<User> {
+    try {
+      return await this.userRepository.findOneOrFail({
+        select: { ...this.userSelect, lessonsCompleted: true },
+        where: { id: userId },
+        relations: { ...this.userRelationships, lessonsCompleted: true },
+      });
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
+  }
+
   async getUserById(userId: number): Promise<User> {
-    return await this.userRepository.findOneOrFail({
-      select: {
-        id: true,
-        userName: true,
-        email: true,
-        createdLessons: true,
-      },
-      where: { id: userId },
-      relations: { createdLessons: true },
-    });
+    try {
+      return await this.userRepository.findOneOrFail({
+        select: this.userSelect,
+        where: { id: userId },
+        relations: this.userRelationships,
+      });
+    } catch (err) {
+      throw new BadRequestException(err);
+    }
   }
 }
