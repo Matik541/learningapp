6a06e9973b3e61c705f221494d0098e4316b3c0c
Marian Horoshko <marian.horoshko@gmail.com>
2023-10-31 17:35:59 +0100

    add delete route test for flashcards controller

--- src/flashcards/flashcards.controller.spec.ts
+++ src/flashcards/flashcards.controller.spec.ts
@@ -7,6 +7,7 @@ import { FlashcardsService } from './flashcards.service';
 import { mockFlashcardsService } from './mock/flashcards.service';
 
 import { AddFlashcardDto } from './dto/addFlashcard.dto';
+import { UpdateFlashcardDto } from './dto/updateFlashcard.dto';
 
 describe('FlashcardsController', () => {
   let controller: FlashcardsController;
@@ -52,6 +53,41 @@ describe('FlashcardsController', () => {
       const err = await validate(addFlashcardsWithBadData);
 
       expect(err.length).toBe(2);
+    });
+  });
+
+  describe('update flashcards route', () => {
+    it('should update lesson flashcard', async () => {
+      expect(
+        await controller.updateFlashcard(1, 1, 1, {
+          question: 'updated flascard',
+          answer: 'test',
+        }),
+      ).toEqual({
+        id: 1,
+        question: 'updated flascard',
+        answer: 'test',
+        lesson: { creator: { id: 1 } },
+      });
+    });
+
+    it('should fail validation', async () => {
+      const updateFlashcardsWithBadData: UpdateFlashcardDto =
+        new UpdateFlashcardDto();
+      updateFlashcardsWithBadData.answer = '';
+
+      const err = await validate(updateFlashcardsWithBadData);
+
+      expect(err.length).toBe(2);
+    });
+  });
+
+  it('should delete flashcard by id', async () => {
+    expect(await controller.removeFlashcard(1, 1, 1)).toEqual({
+      id: 1,
+      question: 'updated flascard',
+      answer: 'test',
+      lesson: { creator: { id: 1 } },
     });
   });
 });
--- src/flashcards/mock/flashcards.service.ts
+++ src/flashcards/mock/flashcards.service.ts
@@ -5,6 +5,7 @@ import { Flashcard } from '../entities/flashcard.entity';
 import { Lesson } from 'src/lessons/entities/lesson.entity';
 
 import { AddFlashcardDto } from '../dto/addFlashcard.dto';
+import { UpdateFlashcardDto } from '../dto/updateFlashcard.dto';
 
 let flashcards: Flashcard[] = [];
 
@@ -13,6 +14,8 @@ mockUser.id = 1;
 
 const mockLesson = new Lesson();
 mockLesson.creator = mockUser;
+
+let id = 1;
 
 export const mockFlashcardsService = {
   addFlashcardsToLesson: jest
@@ -28,15 +31,69 @@ export const mockFlashcardsService = {
 
         flashcards = flashcardsData.map((card) => {
           const flashcard = new Flashcard();
-          flashcard.id = 1;
+          flashcard.id = id;
           flashcard.question = card.question;
           flashcard.answer = card.answer;
           flashcard.lesson = mockLesson;
+
+          id++;
 
           return flashcard;
         });
 
         return Promise.resolve(flashcards);
+      },
+    ),
+  updateFlashcard: jest
+    .fn()
+    .mockImplementation(
+      async (
+        lessonCreatorId: number,
+        lessonId: number,
+        flashcardId: number,
+        updateFlashcardDto: UpdateFlashcardDto,
+      ): Promise<Flashcard> => {
+        if (mockLesson.creator.id !== lessonCreatorId)
+          throw new BadRequestException('Invalid user');
+
+        let res: Flashcard;
+
+        flashcards = flashcards.map((f) => {
+          if (f.id == flashcardId) {
+            f = {
+              ...f,
+              question: updateFlashcardDto.question,
+              answer: updateFlashcardDto.answer,
+            };
+
+            res = f;
+
+            return f;
+          }
+        });
+
+        return Promise.resolve(res);
+      },
+    ),
+
+  removeFlashcard: jest
+    .fn()
+    .mockImplementation(
+      async (
+        lessonCreatorId: number,
+        lessonId: number,
+        flashcardId: number,
+      ): Promise<Flashcard> => {
+        if (mockLesson.creator.id !== lessonCreatorId)
+          throw new BadRequestException('Invalid user');
+
+        const flashcard = flashcards.find(
+          (flashcard) => flashcard.id === flashcardId,
+        );
+
+        flashcards = flashcards.filter((f) => f !== flashcard);
+
+        return Promise.resolve(flashcard);
       },
     ),
 };
