130df1d61754cfe76690bc6084226ff7ebc25518
Matik - PC <mateuszkowalski199700@gmail.com>
2023-11-08 11:48:16 +0100

    create lesson - update flashcard list, add fleshcard, fix reset, remove tags placeholder

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,11 +1,14 @@
-import { TagsService } from './../../services/tags.service'
-import { COMMA, ENTER } from '@angular/cdk/keycodes'
-import { Flashcard, Tag } from 'src/app/enums/enums'
-import { Component, ElementRef, ViewChild } from '@angular/core'
-import { FormBuilder, FormControl, Validators } from '@angular/forms'
-import { MatChipInputEvent } from '@angular/material/chips'
-import { Observable, of } from 'rxjs'
-import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete'
+import { TagsService } from './../../services/tags.service';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Flashcard, Tag } from 'src/app/enums/enums';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatChipInputEvent } from '@angular/material/chips';
+import { Observable, map, of } from 'rxjs';
+import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { StepperOrientation } from '@angular/cdk/stepper';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-create',
@@ -13,101 +16,132 @@ import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete'
   styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent {
-  rowCount = 1
-  separatorKeysCodes: number[] = [ENTER, COMMA]
+  rowCount = 1;
+  separatorKeysCodes: number[] = [ENTER, COMMA];
 
   detailsFormGroup = this.formBuilder.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
     tagsCtrl: [''],
-  })
+  });
   flashcardFormGroup = this.formBuilder.group({
-    secondCtrl: ['', Validators.required],
-  })
+    question: ['', Validators.required],
+    answer: ['', Validators.required],
+  });
 
-  filteredTags: Observable<Tag[]> = of([])
-  tags: Tag[] = []
-  allTags: Tag[] = [
-    {
-      id: 0,
-      tagName: 'tag1',
-    },
-    {
-      id: 1,
-      tagName: 'tag2',
-    },
-  ]
+  filteredTags: Observable<Tag[]> = of([]);
+  tags: Tag[] = [];
+  allTags: Tag[] = [];
 
-  @ViewChild('tagInput') tagInput!: ElementRef
+  flashcards: Flashcard[] = [];
+
+  @ViewChild('tagInput') tagInput!: ElementRef;
+  @ViewChild('stepper', {read:MatStepper}) stepper!: MatStepper;
+  
+  stepperOrientation: Observable<StepperOrientation>;
 
   constructor(
     private formBuilder: FormBuilder,
     private tagsService: TagsService,
+    breakpointObserver: BreakpointObserver
   ) {
-    tagsService.getTags().subscribe((tags) => {
-      console.log(tags)
-      this.allTags = tags
-      this.filteredTags = of(tags.slice(0, 5))
-    })
+    this.tagsService.getTags().subscribe((tags) => {
+      console.log(tags);
+      this.allTags = tags;
+      this.filteredTags = of(tags.slice(0, 5));
+    });
 
     this.detailsFormGroup.valueChanges.subscribe((form) => {
-      this.filteredTags = of(this._filter(form.tagsCtrl))
-    })
+      this.filteredTags = of(this._filter(form.tagsCtrl));
+    });
 
     this.detailsFormGroup.valueChanges.subscribe((value) => {
-      this.rowCount = value.description?.split(/\n/g).length || 1
-    })
-  }
+      this.rowCount = value.description?.split(/\n/g).length || 1;
+    });
+
+    this.stepperOrientation = breakpointObserver
+      .observe('(min-width: 800px)')
+      .pipe(map(({matches}) => (matches ? 'horizontal' : 'vertical')));
+    }
 
   addTag(event: MatChipInputEvent): void {
-    const input = event.input
-    const value = event.value
+    const input = event.input;
+    const value = event.value;
 
     let inputed = this.allTags.filter(
       (item) =>
         item.tagName.toLowerCase().includes(value.toLocaleLowerCase()) &&
-        !this.tags.includes(item),
-    )[0]
+        !this.tags.includes(item)
+    )[0];
 
     if (inputed && !this.tags.includes(inputed)) {
-      this.tags.push(inputed)
+      this.tags.push(inputed);
     }
 
     if (input) {
-      input.value = ''
+      input.value = '';
     }
 
-    this.detailsFormGroup.controls.tagsCtrl.setValue(null)
+    this.detailsFormGroup.controls.tagsCtrl.setValue(null);
   }
 
   removeTag(tag: Tag): void {
-    const index = this.tags.indexOf(tag)
+    const index = this.tags.indexOf(tag);
 
     if (index >= 0) {
-      this.tags.splice(index, 1)
+      this.tags.splice(index, 1);
     }
 
-    this.filteredTags = of(this._filter(this.detailsFormGroup.value.tagsCtrl))
+    this.filteredTags = of(this._filter(this.detailsFormGroup.value.tagsCtrl));
   }
 
   selectedTag(event: MatAutocompleteSelectedEvent): void {
     let selected = this.allTags.find(
-      (item) => item.tagName === event.option.viewValue,
-    )
+      (item) => item.tagName === event.option.viewValue
+    );
     if (selected && !this.tags.includes(selected)) {
-      this.tags.push(selected)
+      this.tags.push(selected);
     }
 
-    this.filteredTags = of(this._filter(this.detailsFormGroup.value.tagsCtrl))
+    this.filteredTags = of(this._filter(this.detailsFormGroup.value.tagsCtrl));
 
-    this.detailsFormGroup.controls.tagsCtrl.setValue(null)
-    this.tagInput.nativeElement.value = ''
+    this.detailsFormGroup.controls.tagsCtrl.setValue(null);
+    this.tagInput.nativeElement.value = '';
   }
 
   private _filter(value: string | undefined | null): Tag[] {
     return this.allTags.filter(
       (item) =>
-        item.tagName.toLowerCase().includes(value ?? "") && !this.tags.includes(item),
-    )
+        item.tagName.toLowerCase().includes(value ?? '') &&
+        !this.tags.includes(item)
+    );
   }
+
+  addFlashcard() {
+    console.log(this.flashcardFormGroup.value)
+
+    if (this.flashcardFormGroup.invalid) return;
+
+    let flashcard = this.flashcardFormGroup.value as Flashcard;
+
+    console.log(flashcard);
+
+    this.flashcards.push(flashcard);
+    this.flashcardFormGroup.reset();
+  }
+
+  removeFlashcard(flashcard: Flashcard) {
+    let index = this.flashcards.indexOf(flashcard);
+
+    if (index >= 0) {
+      this.flashcards.splice(index, 1);
+    }
+  }
+
+  reset() {
+    this.flashcards = [];
+    this.stepper.reset()
+  } 
+
+  create() {}
 }
