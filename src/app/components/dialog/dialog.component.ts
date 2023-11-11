605cf8e121e2da708c208f85ab794ff646f3b91c
Matik - PC <mateuszkowalski199700@gmail.com>
2023-11-11 22:40:29 +0100

    Add dialog component

---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss'],
+})
+export class DialogComponent {
+  constructor(
+    @Inject(MAT_DIALOG_DATA)
+    public data: {
+      title: string;
+      content: string;
+
+      confirm?: {
+        text: string;
+        color?: string;
+        type?: 'raised' | 'stroked' | 'flat';
+      };
+      cancel?: {
+        text: string;
+        color?: string;
+        type?: 'raised' | 'stroked' | 'flat';
+      };
+    }
+  ) {}
+
+  confirm() {
+    return true;
+  }
+  cancel() {
+    return false;
+  }
+}
